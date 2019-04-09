76bd58c3daab1f9b71cbf760e0794c11f052f9c2
Mark Green <pavedroad@comlv.com>
2019-04-09 10:54:11 -0700

    Remove echo commands and unneeded setting of stateroot

--- salt/apply-state.sh
+++ salt/apply-state.sh
@@ -2,40 +2,34 @@
 
 # TBD trap on error
 
-# for debug
+# for debugging
 # showgrains=1
-# echo=echo
 # dryrun="test=True"
 # loglevel=debug
 loglevel=info
 
 saltdir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" &>/dev/null && pwd)
 
-${echo} salt-call                       \
+salt-call \
     --config-dir "${saltdir}/config/" \
     grains.setval username $(whoami)
 
-${echo} salt-call                       \
+salt-call \
     --config-dir "${saltdir}/config/" \
     grains.setval homedir $HOME
 
-${echo} salt-call                       \
-    --config-dir "${saltdir}/config/" \
-    grains.setval stateroot ${saltdir}/states
-
-${echo} salt-call                       \
+salt-call \
     --config-dir "${saltdir}/config/" \
     grains.setval saltenv dev
 
-${echo} salt-call                       \
+salt-call \
     --config-dir "${saltdir}/config/" \
     --file-root  "${saltdir}/states/" \
-    --log-level  $loglevel \
+    --log-level  ${loglevel} \
     state.highstate ${dryrun}
 
-if [ $showgrains ]; then
-    salt-call                             \
+if [ ${showgrains} ]; then
+    salt-call \
         --config-dir "${saltdir}/config/" \
-        --file-root  "${saltdir}/states/" \
         grains.items
 fi
