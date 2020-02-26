78902cea707917b98426f32388ec35175ad1f686
Mark Green <mark@pavedroad.io>
2020-02-26 08:56:39 -0800

    Make bootstrap options consistent in docker and vagrant

--- devkit/docker/build-fedora29.sh
+++ devkit/docker/build-fedora29.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Build context is set to parent directory that contains bootstrap script
-# Options may be passed to the bootstrap script by setting ARGS
-# ARGS="--build-arg OPTS=-s"
-docker build --tag fedora29:pavedroad --file Dockerfile-fedora29 .. ${ARGS}
+# Options for the bootstrap script are passed in the BOOT_OPTS environment variable
+# export BOOT_OPTS="-s"
+# export BOOT_OPTS="-b <branch-name>"
+# export BOOT_OPTS="-s -b <branch-name>"
+BOOT_ARGS="--build-arg BOOT_OPTS"
+docker build --tag fedora29:pavedroad --file Dockerfile-fedora29 ${BOOT_ARGS} ..
--- devkit/vagrant/eoan/startup-eoan.sh
+++ devkit/vagrant/eoan/startup-eoan.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Options for the bootstrap script may be passed in BOOT_OPTS environment variable
-# BOOT_OPTS="-s" vagrant up
-# BOOT_OPTS="-b <branch-name>" vagrant up
+# Options for the bootstrap script are passed in the BOOT_OPTS environment variable
+# export BOOT_OPTS="-s"
+# export BOOT_OPTS="-b <branch-name>"
+# export BOOT_OPTS="-s -b <branch-name>"
 vagrant up
