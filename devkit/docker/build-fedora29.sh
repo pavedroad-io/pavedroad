d4b43abf5528acbb66f0da87a81233f032101d57
Mark Green <mark@pavedroad.io>
2020-07-12 08:00:07 -0700

    Remove Dockerfiles for OSes no longer supported

--- devkit/docker/build-fedora29.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Build context is set to parent directory that contains bootstrap script
-# Options for the bootstrap script are passed in the BOOT_OPTS environment variable
-# export BOOT_OPTS="-s"
-# export BOOT_OPTS="-b <branch-name>"
-# export BOOT_OPTS="-s -b <branch-name>"
-BOOT_ARGS="--build-arg BOOT_OPTS"
-docker build --tag fedora29:pavedroad --file Dockerfile-fedora29 ${BOOT_ARGS} ..
--- devkit/docker/build-opensuse15.1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Build context is set to parent directory that contains bootstrap script
-# Options for the bootstrap script are passed in the BOOT_OPTS environment variable
-# export BOOT_OPTS="-s"
-# export BOOT_OPTS="-b <branch-name>"
-# export BOOT_OPTS="-s -b <branch-name>"
-BOOT_ARGS="--build-arg BOOT_OPTS"
-docker build --tag opensuse15.1:pavedroad --file Dockerfile-opensuse15.1 ${BOOT_ARGS} ..
