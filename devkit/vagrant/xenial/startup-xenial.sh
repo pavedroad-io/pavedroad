7e02d5c6d188fb7dbeec65118132229be3be3bef
Mark Green <mark@pavedroad.io>
2020-07-12 08:10:39 -0700

    Remove Vagrantfiles for OS versions no longer supported

--- devkit/vagrant/xenial/startup-xenial.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Options for the bootstrap script are passed in the BOOT_OPTS environment variable
-# export BOOT_OPTS="-s"
-# export BOOT_OPTS="-b <branch-name>"
-# export BOOT_OPTS="-s -b <branch-name>"
-vagrant up
