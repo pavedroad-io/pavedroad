ade399ce4da0f8d737a6631a9d9b3e1b646ba423
Mark Green <mark@pavedroad.io>
2020-07-12 08:01:09 -0700

    Add Dockerfiles for new OS versions supported

---
+++ devkit/docker/build-opensuse15.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Build context is set to parent directory that contains bootstrap script
+# Options for the bootstrap script are passed in the BOOT_OPTS environment variable
+# export BOOT_OPTS="-s"
+# export BOOT_OPTS="-b <branch-name>"
+# export BOOT_OPTS="-s -b <branch-name>"
+BOOT_ARGS="--build-arg BOOT_OPTS"
+docker build --tag opensuse15:pavedroad --file Dockerfile-opensuse15 ${BOOT_ARGS} ..
